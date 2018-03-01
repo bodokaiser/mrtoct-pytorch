fdb53d8c9b5f03e8b6f428251930e1dbd7a52808
Bodo Kaiser <i@bodokaiser.io>
2018-03-01 16:56:40 +0100

    added model save, load support to train

--- train.py
+++ train.py
@@ -1,3 +1,6 @@
+import os
+import time
+import torch
 import argparse
 
 from torch.nn import L1Loss
@@ -5,10 +8,27 @@ from torch.optim import Adam
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose
+from torchvision.utils import save_image
 
 from mrtoct.network import Generator
 from mrtoct.dataset import HDF5, Combined
 from mrtoct.transform import Normalize, ToTensor
+
+
+def timestamp():
+  return time.strftime('%d%m%Y%H%M%S')
+
+
+def save_model(model, checkpoint_path):
+  filename = os.path.join(checkpoint_path, f'{timestamp()}.pt')
+
+  torch.save(model.state_dict(), filename)
+
+
+def save_results(inputs, outputs, targets, checkpoint_path):
+  filename = os.path.join(checkpoint_path, f'{timestamp()}.jpg')
+
+  save_image(torch.stack([inputs[0], outputs[0], targets[0]]), filename)
 
 
 def main(args):
@@ -17,6 +37,9 @@ def main(args):
 
   if args.cuda:
     model = model.cuda()
+  if args.checkpoint:
+    model.load_state_dict(torch.load(os.path.join(
+        args.checkpoint_path, args.checkpoint)))
 
   inputs_dataset = HDF5(args.inputs_path, 'slices')
   inputs_transform = Compose([
@@ -54,8 +77,12 @@ def main(args):
       loss.backward()
       optimizer.step()
 
-      if step % 100 == 0:
+      if step % args.log_steps == 0:
         print(f'step: {step}, loss: {loss.data[0]}')
+
+        save_image(inputs, outputs, targets, args.checkpoint_path)
+      if step % args.save_steps == 0:
+        save_model(model, args.checkpoint_path)
 
 
 if __name__ == '__main__':
@@ -65,6 +92,9 @@ if __name__ == '__main__':
   parser.add_argument('--learn-rate', default=2e-4)
   parser.add_argument('--beta1-rate', default=5e-1)
   parser.add_argument('--num-epochs', default=300)
+  parser.add_argument('--log-steps', default=100)
+  parser.add_argument('--save-steps', default=500)
+  parser.add_argument('--checkpoint')
   parser.add_argument('--inputs-path')
   parser.add_argument('--targets-path')
   parser.add_argument('--checkpoint-path')
